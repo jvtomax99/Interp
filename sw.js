0b04da28f5a5496cacf8e66fb4aafb22c2748e4b
jvtomax99 <jvtomax99@gmail.com>
2026-09-22 05:30:44 -0400

    Raise the Panorama photo slightly on phones and desktops to move the hospital sign above the blue mist.

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@
  * the scenario this cache exists for in the first place).
  */
 
-const CACHE_VERSION = 'interpreter-hub-v99-banner-sign-framing';
+const CACHE_VERSION = 'interpreter-hub-v100-raised-banner-photo';
 const PRECACHE = [
   './',
   './index.html',
